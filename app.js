c5302122614c9ee2a04c0782c3f7369943b0e29a
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-05-02 09:38:45 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            firstName: "Gabriel",
+            lastName: "Rockson",
+            email: "thegabrielrockson@gmail.com",
+            gender: "male",
+            picture: './pic.jpg',
+        }
+    },
+    methods: {
+        async getUser() {
+
+            const res = await fetch('https://randomuser.me/api');
+            const { results } = await res.json();
+
+            this.firstName = results[0].name.first
+            this.lastName = results[0].name.last
+            this.email = results[0].email
+            this.gender = results[0].gender
+            this.picture = results[0].picture.large
+        }
+    }
+})
+
+
+app.mount("#app")
